56ed184b80b15cd5e2a6685085be884f99ff8856
rldnd <gi981226@gmail.com>
2024-05-02 13:54:36 +0900

    chore(pkg): eslint setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+
+import reactLogo from "@/assets/react.svg";
+
+import viteLogo from "public/vite.svg";
+
+import styles from "./test.module.scss";
 
 function App() {
 	const [count, setCount] = useState(0);
 
+	useEffect(() => {
+		console.log(count);
+	}, [count]);
+
 	return (
 		<>
 			<div>
-				<a href="https://vitejs.dev" target="_blank">
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://react.dev" target="_blank">
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const PORT = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	server: {
 		port: PORT,
 	},
