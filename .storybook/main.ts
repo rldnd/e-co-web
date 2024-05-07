a7a602e8c532c9de9394407a69b70c2219e933fd
rldnd <gi981226@gmail.com>
2024-05-07 10:47:11 +0900

    chore(storybook): setting

---
+++ .storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-onboarding",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	core: {
+		builder: {
+			name: "@storybook/builder-vite",
+			options: {
+				viteConfigPath: "vite.config.ts",
+			},
+		},
+	},
+	typescript: {
+		reactDocgen: "react-docgen-typescript",
+	},
+};
+export default config;
