7080697ae0ecd5319a3d9ebe189441686943a8f8
rldnd <gi981226@gmail.com>
2024-05-07 13:17:08 +0900

    feat(sentry): sentry init

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+import * as Sentry from "@sentry/react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import "./index.css";
+
+if (import.meta.env.VITE_SENTRY_DSN) {
+	Sentry.init({
+		dsn: import.meta.env.VITE_SENTRY_DSN as string,
+		integrations: [
+			Sentry.browserTracingIntegration(),
+			Sentry.replayIntegration(),
+		],
+		tracesSampleRate: 1.0,
+		tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+		replaysSessionSampleRate: 0.1,
+		replaysOnErrorSampleRate: 1.0,
+	});
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
